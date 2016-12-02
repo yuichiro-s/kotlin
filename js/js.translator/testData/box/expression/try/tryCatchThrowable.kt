3d2de6ec49415c253324d4de677f77b3f4e71c93
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2016-12-02 15:40:49 +0300

    JS: don't omit guard for catch with Throwable type (KT-13616); don't generate guard for catch with dynamic type (KT-13615)

--- js/js.translator/testData/box/expression/try/tryCatchThrowable.kt
+++ js/js.translator/testData/box/expression/try/tryCatchThrowable.kt
@@ -11,5 +11,36 @@ fun box(): String {
     }
     assertEquals("Throwable", s)
 
+    try {
+        js("throw null")
+    } catch (e: Throwable) {
+        s = "Throwable"
+    } catch (e: dynamic) {
+        s = "dynamic"
+    }
+    assertEquals("dynamic", s)
+
+
+    try {
+        try {
+            js("throw null")
+        }
+        catch (e: Throwable) {
+            s = "Throwable"
+        }
+    } catch (e: dynamic) {
+        s = "dynamic2"
+    }
+    assertEquals("dynamic2", s)
+
+
+    try {
+        js("throw Object.create(null)")
+    }
+    catch (e: dynamic) {
+        s = "dynamic3"
+    }
+    assertEquals("dynamic3", s)
+
     return "OK"
 }
