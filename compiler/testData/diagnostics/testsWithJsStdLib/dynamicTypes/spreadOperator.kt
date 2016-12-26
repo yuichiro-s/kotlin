3d6e8a12cd2f9233cfabf205b2aa6687ca00de4b
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-12-26 17:44:59 +0300

    JS: prohibit spread operator and destructuring declaration in `dynamic` values. See KT-15283

--- compiler/testData/diagnostics/testsWithJsStdLib/dynamicTypes/spreadOperator.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/dynamicTypes/spreadOperator.kt
@@ -1,14 +1,14 @@
 fun test(d: dynamic) {
     val a = arrayOf(1, 2, 3)
 
-    d.foo(*d)
-    d.foo(*a)
-    d.foo(1, "2", *a)
-    d.foo(1, *a) { }
-    d.foo(*a) { "" }
-    d.foo(*a, *a)
-    d.foo(*a, *a) { "" }
-    d.foo(*a, 1, { "" }, *a)
-    d.foo(*a, 1)
-    d.foo(*a, *a, { "" })
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>d)
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a)
+    d.foo(1, "2", <!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a)
+    d.foo(1, <!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a) { }
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a) { "" }
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a, <!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a)
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a, <!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a) { "" }
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a, 1, { "" }, <!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a)
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a, 1)
+    d.foo(<!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a, <!SPREAD_OPERATOR_IN_DYNAMIC_CALL!>*<!>a, { "" })
 }
