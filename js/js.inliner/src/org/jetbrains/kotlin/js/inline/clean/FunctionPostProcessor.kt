9382540abd7e6881400b13df104966940069404b
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-01-19 18:53:56 +0300

    JS: transfer all empty statement simplifications RedundantLabelRemoval to EmptyStatementElimination, so that empty statements can be eliminated everywhere, not in labeled blocks only.

--- js/js.inliner/src/org/jetbrains/kotlin/js/inline/clean/FunctionPostProcessor.kt
+++ js/js.inliner/src/org/jetbrains/kotlin/js/inline/clean/FunctionPostProcessor.kt
@@ -22,6 +22,7 @@ class FunctionPostProcessor(root: JsFunction) {
     val optimizations = listOf(
         //{ TemporaryAssignmentElimination(root.body).apply() },
         { RedundantLabelRemoval(root.body).apply() },
+        { EmptyStatementElimination(root.body).apply() },
         { WhileConditionFolding(root.body).apply() },
         { DoWhileGuardElimination(root.body).apply() },
         { TemporaryVariableElimination(root).apply() },
